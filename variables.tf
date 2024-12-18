9f1cb2ac10e99bff17bb9d4bc4fb342554086443
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-18 14:51:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "A list of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "A list of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "region" {
+  description = "The AWS region to deploy the VPC"
+  type        = string
+  default     = "ap-south-1"
+}
