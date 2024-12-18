9365b6a605ef8bf7c471a3174626046563f56d56
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-18 16:46:14 +0530

    Delete remote_backend.tf

--- remote_backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket-for-module-13234"
-    key            = "pradeep/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-demo"
-  }
-}
